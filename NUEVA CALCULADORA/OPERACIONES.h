a46532d32dd39acb6604abaa00c1fe8cd2c6bd77
comandos h <hernansanchez_91@hotmail.com.ar>
2020-09-05 16:05:57 -0300

    Repositorio Agosto 2020

--- NUEVA CALCULADORA/OPERACIONES.h
+++
@@ -1,7 +0,0 @@
-
-
-float suma(float numero1,float numero2);
-int resta(int numero1,int numero2);
-int division(int numero1,int numero2);
-int multiplicacion(int numero1,int numero2);
-int factorial(int numero1);
