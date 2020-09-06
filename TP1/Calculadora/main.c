48ead1200048508c2096a8c502649b99fdb5fee8
comandos h <hernansanchez_91@hotmail.com.ar>
2020-09-05 21:21:53 -0300

    Repositorio Agosto 2020

--- TP1/Calculadora/main.c
+++ TP1/Calculadora/main.c
@@ -1,54 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+int suma(int num1, int num2);
+
+
+
 int main()
 {
     int num1;
     int num2;
     int respuesta;
     int flag=0;
-    //int resultado;
+    int resultado;
+
+
 
     do
     {
 
 
-        printf(" 1.  Ingresar 1er operando (A=x) ");
+        printf(" 1.  Ingresar 1er operando ");
 
-        printf("\n 2.  Ingresar 2er operando (B=y) ");
+        printf("\n 2.  Ingresar 2er operando ");
 
-        printf("\n 3.  Calcular todas las operaciones\n\n  a) Calcular la suma (A+B)\n  b) Calcular la resta (A-B)\n  c) Calcular la division (A/B)\n  d) Calcular la multiplicacion (A*B)\n  e) Calcular el factorial (A!)\n");
+        printf("\n 3.  Calcular todas las operaciones");
 
-        printf("\n 4.  Informar resultados\n\n  a) El resultado de A+B es: r \n  b) El resultado de A-B es: r\n  c) El resultado de A/B es: r o No es posible dividir por cero\n  d) El resultado de A*B es: r\n  e) El factorial de A es: r1 y El factorial de B es: r2\n ");
+        printf("\n 4.  Informar resultados");
 
         printf("\n 5.  Salir\n");
 
         printf("\nIngrese una opcion --> 1/2/3/4/5\n");
         scanf("%d", &respuesta);
-
-
         system("cls");
-
 
         switch(respuesta)
         {
         case 1:
-            printf(" 1.  Ingresar 1er operando (A=x)= ");
-            scanf("%d",&num1);
-            system("PAUSE");
-            system("cls");
-            break;
 
+            if(flag==0)
+            {
+                printf(" 1.  Ingresar 1er operando (A=x)= ");
+                scanf("%d",&num1);
+                system("cls");
+                break;
+
+            }
 
         case 2:
-            printf(" 2.  Ingresar 2do operando (B=y)= ");
-            scanf("%d",&num2);
-            system("PAUSE");
-            system("cls");
-            break;
+
+            if(flag==0)
+            {
+                printf(" 2.  Ingresar 2do operando (B=y)= ");
+                scanf("%d",&num2);
+                system("cls");
+                break;
+            }
+
+
 
         case 3:
             if(flag==0)
+            {
+
+                printf(" 1.  Ingresar 1er operando (%d) ",num1);
+
+                printf("\n 2.  Ingresar 2er operando (%d) ", num2);
+
+                printf("\n 3.  Calcular todas las operaciones\n\n  a) Calcular la suma (%d+%d)\n  b) Calcular la resta (%d-%d)\n  c) Calcular la division (%d/%d)\n  d) Calcular la multiplicacion (%d*%d)\n  e) Calcular el factorial (%d!) (%d!)\n",num1,num2,num1,num2,num1,num2,num1,num2,num1,num2);
+
+                printf("\n 4.  Informar resultados\n");
+
+                printf("\n 5.  Salir\n\n");
+
+                flag=1;
+                system("PAUSE");
+                system("cls");
+                break;
+
+            }
+
+            else
             {
                 printf("No hay numeros para Operar\n");
                 system("PAUSE");
@@ -56,32 +88,40 @@ int main()
                 break;
             }
 
+        case 4:
+            if(flag==0)
+            {
+                printf("No hay Resultados para Mostrar\n");
+                system("PAUSE");
+                system("cls");
+                break;
+
+            }
             else
             {
-                printf(" 1.  Ingresar 1er operando (%d=x) ",num1);
+                resultado= suma(num1,num2);
 
-                printf("\n 2.  Ingresar 2er operando (B=y) ");
-
-                printf("\n 3.  Calcular todas las operaciones\n\n  a) Calcular la suma (A+B)\n  b) Calcular la resta (A-B)\n  c) Calcular la division (A/B)\n  d) Calcular la multiplicacion (A*B)\n  e) Calcular el factorial (A!)\n");
-
-                printf("\n 4.  Informar resultados\n\n  a) El resultado de A+B es: r \n  b) El resultado de A-B es: r\n  c) El resultado de A/B es: r o No es posible dividir por cero\n  d) El resultado de A*B es: r\n  e) El factorial de A es: r1 y El factorial de B es: r2\n ");
-
-                printf("\n 5.  Salir\n");
-
-
-
-
+                printf(" a) El resultado de %d+%d es %d \n", num1, num2, resultado);
+                system("PAUSE");
+                system("cls");
+                break;
 
             }
 
-
-
         }
-
 
     }
     while(respuesta!=5);
 
 
     return 0;
+}
+
+int suma(int num1, int num2)
+{
+    int resultado;
+
+    resultado= num1 + num2;
+
+    return resultado;
 }
